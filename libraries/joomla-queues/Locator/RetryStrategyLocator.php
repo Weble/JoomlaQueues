3f0e9e4ec17e910e5522d88c5aa2d7b51b5986f9
Skullbock <daniele@weble.it>
2020-03-23 08:55:21 +0100

    configurable retry strategy

--- libraries/joomla-queues/Locator/RetryStrategyLocator.php
+++ libraries/joomla-queues/Locator/RetryStrategyLocator.php
@@ -3,6 +3,7 @@
 
 namespace Weble\JoomlaQueues\Locator;
 
+use Joomla\CMS\Component\ComponentHelper;
 use Psr\Container\ContainerInterface;
 use Symfony\Component\Messenger\Retry\MultiplierRetryStrategy;
 
@@ -10,7 +11,13 @@ class RetryStrategyLocator implements ContainerInterface
 {
     public function get($id)
     {
-        return new MultiplierRetryStrategy();
+        $config = ComponentHelper::getParams('com_queues');
+        return new MultiplierRetryStrategy(
+            $config->get('max_retries', 3),
+            $config->get('retry_delay', 1000),
+            $config->get('retry_multiplier', 1),
+            $config->get('max_retry_delay', 0)
+        );
     }
 
     public function has($id)
